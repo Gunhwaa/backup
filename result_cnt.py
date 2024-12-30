ca6fc9dc0c7cef9f29d38c5e1205b9329f1f2d5f
Gunhwaa <hwag00123@gmail.com>
2024-12-30 20:58:57 +0900

    Initial commit

--- result_cnt.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-
-# open
-with open('work_dirs/vis/tr3d/1/tr3d03_result.pkl', 'rb') as f :
-    data = pickle.load(f)
-print(data)
--- test_tr3d-ff_5.sh
+++
@@ -1,9 +0,0 @@
-CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=2 python tools/test.py configs/tr3d/tr3d_sunrgbd-3d-10class.py work_dirs/tr3d/head01/5/epoch_12.pth --eval mAP 
-
-CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=2 python tools/test.py configs/tr3d/tr3d_sunrgbd-3d-10class.py work_dirs/tr3d/head01/5/epoch_12.pth --eval mAP 
-
-CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=2 python tools/test.py configs/tr3d/tr3d_sunrgbd-3d-10class.py work_dirs/tr3d/head01/5/epoch_12.pth --eval mAP 
-
-CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=2 python tools/test.py configs/tr3d/tr3d_sunrgbd-3d-10class.py work_dirs/tr3d/head01/5/epoch_12.pth --eval mAP 
-
-CUDA_VISIBLE_DEVICES=4 OMP_NUM_THREADS=2 python tools/test.py configs/tr3d/tr3d_sunrgbd-3d-10class.py work_dirs/tr3d/head01/5/epoch_12.pth --eval mAP 
